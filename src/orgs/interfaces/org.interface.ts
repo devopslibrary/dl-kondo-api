3eb3a7af0aa89eea344fa83c1666dba85dd06651
Ken Erwin <ken@devopslibrary.com>
2019-12-06 13:16:57 -0500

    Feature/set up graphql (#1)
    * OK we are working now
    
    * getting there
    
    * can add via GraphQL, cannot retrieve all yet
    
    * a few unnecessary lines

--- src/orgs/interfaces/org.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Org {
-  name: string;
-  age: number;
-  breed: string;
-}
--- src/orgs/orgs.module.ts
+++ src/orgs/orgs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrgsController } from './orgs.controller';
 import { OrgsService } from './orgs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Org } from './orgs.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Org])],
   controllers: [OrgsController],
   providers: [OrgsService],
   exports: [OrgsModule],
